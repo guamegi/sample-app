76296a1535118941a890a001fc7cec6088364f54
Minha Kwon <namisoft@naver.com>
2023-08-30 21:38:47 +0900

    글 읽기

--- src/app/read/[id]/page.js
+++ src/app/read/[id]/page.js
@@ -1,4 +1,10 @@
-export default function Read(props) {
-  const id = props.params.id;
-  return <>read {id}</>;
+export default async function Read(props) {
+  const res = await fetch(`http://localhost:9999/topics/${props.params.id}`);
+  const topic = await res.json();
+  return (
+    <>
+      <h2>{topic.title}</h2>
+      {topic.body}
+    </>
+  );
 }
